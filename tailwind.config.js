bb58df836f70a6ee0ae54d60ecc9a39b24ba4b20
Krindog7337 <igstkrisna2006@gmail.com>
2023-09-17 12:10:24 +0800

    initial committ

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./web/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-main": "#fbf8f0",
+        "light-second": "#ffc6a5",
+      },
+    },
+  },
+  plugins: [],
+};
